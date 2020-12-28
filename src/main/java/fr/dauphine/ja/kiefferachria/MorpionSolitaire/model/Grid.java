a897e444717f5369ddc585159409795f5a554205
FloryanKieffer <floryan@mbp-de-floryan.home>
2020-12-28 17:12:38 +0100

    Second commit UI

--- src/main/java/fr/dauphine/ja/kiefferachria/MorpionSolitaire/model/Grid.java
+++ src/main/java/fr/dauphine/ja/kiefferachria/MorpionSolitaire/model/Grid.java
@@ -1,12 +1,117 @@
 package fr.dauphine.ja.kiefferachria.MorpionSolitaire.model;
 
+import java.awt.Point;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class Grid {
 
-	private int[][] points;
-    private List<Line> lines;
+	private boolean[][] points;
+	private ArrayList<Point> tabCoordonnee;
+    private int nbLine;
+    private int nbColumn;
+    private int height;
+    private int width;
+    private int step;
+    
+    public Grid(int h,int w, int step){
+    	this.nbLine=(int)w/step;
+    	this.nbColumn=(int)h/step;
+    	this.step=step;
+    	this.height=h;
+    	this.width=w;
+    	this.points = new boolean [nbLine][nbColumn];
+    	this.tabCoordonnee=new ArrayList<Point>();
+    }
+    
+    
+
+	public int getHeight() {
+		return height;
+	}
+
+
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+
+
+	public int getWidth() {
+		return width;
+	}
+
+
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+
+
+	public boolean[][] getPoints() {
+		return points;
+	}
+
+	public void setPoints(boolean[][] points) {
+		this.points = points;
+	}
+
+	public int getNbLine() {
+		return nbLine;
+	}
+
+	public void setNbLine(int nbLine) {
+		this.nbLine = nbLine;
+	}
+
+	public int getNbColumn() {
+		return nbColumn;
+	}
+
+	public void setNbColumn(int nbColumn) {
+		this.nbColumn = nbColumn;
+	}
+
+	public int getStep() {
+		return step;
+	}
+
+	public void setStep(int step) {
+		this.step = step;
+	}
+
+
+
+	public ArrayList<Point> getTabCoordonnee() {
+		return tabCoordonnee;
+	}
+
+
+
+	public void setTabCoordonnee(ArrayList<Point> tabCoordonnee) {
+		this.tabCoordonnee = tabCoordonnee;
+	}
+	
+	public void catchCoordonnee() {
+		int x=0;
+		int y=0;
+		for(int i = 0 ; i < this.nbLine-1;i++) {
+			y=y+step;
+			for(int j = 0 ; j < this.nbColumn-1; j++) {
+				x=x+step;
+				this.tabCoordonnee.add(new Point(x,y));
+			}
+			x=0;
+			
+		}
+	}
+    
+    
+    		
+    		
     
     
 }
--- src/main/java/fr/dauphine/ja/kiefferachria/MorpionSolitaire/view/GridView.java
+++ src/main/java/fr/dauphine/ja/kiefferachria/MorpionSolitaire/view/GridView.java
@@ -23,24 +23,27 @@ public class GridView extends JPanel {
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.setColor(Color.darkGray);
-		for(int i=0; i <600;i=i+40)
-			g.drawLine(0, 40+i, 600, 40+i);
-		for(int i=0; i <600;i=i+40)
-			g.drawLine( 40+i,0, 40+i,600);
+		for(int i=0; i <this.grid.getWidth();i=i+this.grid.getStep())
+			g.drawLine(0, this.grid.getStep()+i, this.grid.getWidth(), this.grid.getStep()+i);
+		for(int i=0; i <this.grid.getHeight();i=i+this.grid.getStep())
+			g.drawLine( this.grid.getStep()+i,0, this.grid.getStep()+i,this.grid.getHeight());
 		g.setColor(Color.red);
 		g.drawLine(40, 80, 0, 40);
 
 	}
 	
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Morpion Solitaire");// Création d'une frame avec un nom qui sera le nom
+		JFrame frame = new JFrame("Morpion Solitaire");
 		frame.setLayout(new GridLayout());
-								// de la fenetre
-		frame.setSize(new Dimension(600, 600));// Changement de taille de la fenetre
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Action à faire si on clique sur le close
-		Grid grid = new Grid();
-		GridView d = new GridView(grid);// Création de notre nouvelle objet
-		frame.add(d);// On met notre objet dans la fenetre
+		frame.setSize(new Dimension(600, 600));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Grid grid = new Grid(600,600,40);
+		grid.catchCoordonnee();
+		for(int i = 0 ; i<grid.getTabCoordonnee().size();i++) {
+			System.out.println(grid.getTabCoordonnee().get(i));
+		}
+		GridView d = new GridView(grid);
+		frame.add(d);
 		
 		frame.setVisible(true);
 	}
