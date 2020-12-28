034bd16709d3a9e3aa1784dda69bb4f86023c9dc
FloryanKieffer <floryan@MBP-de-Floryan.home>
2020-12-28 14:37:27 +0100

    First commit

---
+++ src/main/java/fr/dauphine/ja/kiefferachria/MorpionSolitaire/App.java
@@ -0,0 +1,13 @@
+package fr.dauphine.ja.kiefferachria.MorpionSolitaire;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
