912499fdc80ded0be1c0387172d5295e33e15322
FloryanKieffer <floryan@mbp-de-floryan.home>
2021-01-21 23:10:10 +0100

    Javadoc (2)

--- src/main/java/fr/dauphine/ja/kiefferachria/MorpionSolitaire/model/Direction.java
+++ src/main/java/fr/dauphine/ja/kiefferachria/MorpionSolitaire/model/Direction.java
@@ -1,5 +1,10 @@
 package fr.dauphine.ja.kiefferachria.MorpionSolitaire.model;
 
+/**
+ * All possible directions of a line.
+ * @author floryan
+ *
+ */
 public enum Direction {
 	HORIZONTAL,VERTICAL,DIAGLEFT,DIAGRIGHT,HORIZONTAL_LEFT,HORIZONTAL_RIGHT,VERTICAL_TOP,VERTICAL_BOTTOM,DIAGLEFT_TOPLEFT,DIAGLEFT_BOTTOMRIGHT,DIAGRIGHT_TOPRIGHT,DIAGRIGHT_BOTTOMLEFT,;
 	
