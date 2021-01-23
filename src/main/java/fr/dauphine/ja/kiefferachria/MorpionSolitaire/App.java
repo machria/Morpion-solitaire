17f075bb8a94d0ab6ea4a3a352c90f59dfd7c4c3
machria <achriamajid@gmail.com>
2021-01-23 17:37:31 +0100

    fix javadoc

--- src/main/java/fr/dauphine/ja/kiefferachria/MorpionSolitaire/App.java
+++ src/main/java/fr/dauphine/ja/kiefferachria/MorpionSolitaire/App.java
@@ -2,7 +2,7 @@ package fr.dauphine.ja.kiefferachria.MorpionSolitaire;
 import fr.dauphine.ja.kiefferachria.MorpionSolitaire.controller.Choice;
 /**
  * Launcher of the main app
- *
+ * @author floryan/majid
  */
 public class App 
 {
